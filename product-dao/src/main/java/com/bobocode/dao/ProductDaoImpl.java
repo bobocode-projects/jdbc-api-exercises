c18133776987de6e171bdbfc6a519ade39e329a3
shtramak <shtramak@gmail.com>
2018-11-13 17:22:17 +0200

    refactor ProductDaoImpl

--- product-dao/src/main/java/com/bobocode/dao/ProductDaoImpl.java
+++ product-dao/src/main/java/com/bobocode/dao/ProductDaoImpl.java
@@ -31,17 +31,9 @@ public class ProductDaoImpl implements ProductDao {
     public void save(Product product) {
         try (Connection connection = dataSource.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(SAVE_SQL, Statement.RETURN_GENERATED_KEYS);
-            statement.setString(1, product.getName());
-            statement.setString(2, product.getProducer());
-            statement.setBigDecimal(3, product.getPrice());
-            Date expirationDate = Date.valueOf(product.getExpirationDate());
-            statement.setDate(4, expirationDate);
+            setStatementParameters(statement, product);
             statement.executeUpdate();
-            ResultSet generatedKey = statement.getGeneratedKeys();
-            if (generatedKey.next()) {
-                long id = generatedKey.getLong(1);
-                product.setId(id);
-            }
+            insertGeneratedKeyToProduct(product, statement);
         } catch (SQLException e) {
             String messagee = String.format("Error saving product: %s", product);
             throw new DaoOperationException(messagee, e.getCause());
@@ -53,18 +45,7 @@ public class ProductDaoImpl implements ProductDao {
         try (Connection connection = dataSource.getConnection()) {
             Statement statement = connection.createStatement();
             ResultSet allRows = statement.executeQuery(FIND_ALL_SQL);
-            List<Product> products = new ArrayList<>();
-            while (allRows.next()) {
-                Product product = new Product();
-                product.setId(allRows.getLong("id"));
-                product.setName(allRows.getString("name"));
-                product.setProducer(allRows.getString("producer"));
-                product.setPrice(allRows.getBigDecimal("price"));
-                product.setExpirationDate(allRows.getDate("expiration_date").toLocalDate());
-                product.setCreationTime(allRows.getTimestamp("creation_time").toLocalDateTime());
-                products.add(product);
-            }
-            return products;
+            return resultSetToListOfProducts(allRows);
         } catch (SQLException e) {
             throw new DaoOperationException("Find all failed", e.getCause());
         }
@@ -77,14 +58,7 @@ public class ProductDaoImpl implements ProductDao {
             statement.setLong(1, id);
             ResultSet resultSet = statement.executeQuery();
             resultSet.next();
-            Product product = new Product();
-            product.setId(resultSet.getLong("id"));
-            product.setName(resultSet.getString("name"));
-            product.setProducer(resultSet.getString("producer"));
-            product.setPrice(resultSet.getBigDecimal("price"));
-            product.setExpirationDate(resultSet.getDate("expiration_date").toLocalDate());
-            product.setCreationTime(resultSet.getTimestamp("creation_time").toLocalDateTime());
-            return product;
+            return productFromResultSet(resultSet);
         } catch (SQLException e) {
             String message = String.format("Product with id = %d does not exist", id);
             throw new DaoOperationException(message, e.getCause());
@@ -93,19 +67,12 @@ public class ProductDaoImpl implements ProductDao {
 
     @Override
     public void update(Product product) {
-        if (product.getId() == null) {
-            throw new DaoOperationException("Cannot find a product without ID");
-        }
+        idRequiresNonNull(product);
         try (Connection connection = dataSource.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(UPDATE_SQL);
-            statement.setString(1, product.getName());
-            statement.setString(2, product.getProducer());
-            statement.setBigDecimal(3, product.getPrice());
-            Date expirationDate = Date.valueOf(product.getExpirationDate());
-            statement.setDate(4, expirationDate);
-            statement.setLong(5, product.getId());
-            int affectedRows = statement.executeUpdate();
-            if (affectedRows == 0) {
+            setStatementParameters(statement, product);
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected == 0) {
                 throw new SQLException();
             }
         } catch (SQLException e) {
@@ -116,19 +83,63 @@ public class ProductDaoImpl implements ProductDao {
 
     @Override
     public void remove(Product product) {
-        if (product.getId() == null) {
-            throw new DaoOperationException("Cannot find a product without ID");
-        }
+        idRequiresNonNull(product);
         try (Connection connection = dataSource.getConnection()) {
             PreparedStatement statement = connection.prepareStatement(REMOVE_BY_ID_SQL);
             statement.setLong(1, product.getId());
-            int affectedRows = statement.executeUpdate();
-            if (affectedRows == 0) {
+            int rowsAffected = statement.executeUpdate();
+            if (rowsAffected == 0) {
                 throw new SQLException();
             }
         } catch (SQLException e) {
             String message = String.format("Product with id = %d does not exist", product.getId());
             throw new DaoOperationException(message, e.getCause());
+        }
+    }
+
+    private void setStatementParameters(PreparedStatement statement, Product product) throws SQLException {
+        statement.setString(1, product.getName());
+        statement.setString(2, product.getProducer());
+        statement.setBigDecimal(3, product.getPrice());
+        Date expirationDate = Date.valueOf(product.getExpirationDate());
+        statement.setDate(4, expirationDate);
+        int numberOfStatementParam = statement.getParameterMetaData().getParameterCount();
+        if (numberOfStatementParam == 5) {
+            statement.setLong(5, product.getId());
+        }
+    }
+
+    private Product productFromResultSet(ResultSet resultSet) throws SQLException {
+        return Product.builder()
+                .id(resultSet.getLong("id"))
+                .name(resultSet.getString("name"))
+                .producer(resultSet.getString("producer"))
+                .price(resultSet.getBigDecimal("price"))
+                .expirationDate(resultSet.getDate("expiration_date").toLocalDate())
+                .creationTime(resultSet.getTimestamp("creation_time").toLocalDateTime())
+                .build();
+    }
+
+    private void insertGeneratedKeyToProduct(Product product, PreparedStatement statement) throws SQLException {
+        ResultSet generatedKey = statement.getGeneratedKeys();
+        if (generatedKey.next()) {
+            long id = generatedKey.getLong(1);
+            product.setId(id);
+        }
+    }
+
+    private List<Product> resultSetToListOfProducts(ResultSet allRows) throws SQLException {
+        List<Product> products = new ArrayList<>();
+        while (allRows.next()) {
+            Product product = productFromResultSet(allRows);
+            products.add(product);
+        }
+        return products;
+    }
+
+    private void idRequiresNonNull(Product product) {
+        if (product.getId() == null) {
+            throw new DaoOperationException("Cannot find a product without ID");
         }
     }
 
